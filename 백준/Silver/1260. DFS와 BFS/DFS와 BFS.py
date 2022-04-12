a3c7c80d81cb7d29c45a05975c17565b2c3a8f50
이승준 <72748734+HiLeeS@users.noreply.github.com>
2022-04-12 16:03:39 +0900

    [Silver II] Title: DFS와 BFS, Time: 644 ms, Memory: 120232 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
@@ -1,37 +1,41 @@
 import sys
 from collections import deque
 
-def dfs(edge, v,visited):
+def dfs(edge, v, visited):
     visited[v] = True
-    print(v,end=" ")
+    print(v, end=' ')
+
     for g in edge:
         if v in g:
-            for i in range(2):
-                if visited[g[i]] != True:
-                    dfs(edge, g[i], visited)
+            for i in g:
+                if visited[i] == False:
+                    dfs(edge, i, visited)
 
 def bfs(edge, v, visited):
-    que = deque([v])
     visited[v] = True
-    while que:
-        s = que.popleft()
-        print(s, end=' ')
+    deq = deque()
+    deq.append(v)
+    while deq:
+
+        a = deq.popleft()
+        print(a, end=' ')
+
         for g in edge:
-            if s in g:
-                for i in range(2):
-                    if visited[g[i]] != True:
-                        que.append(g[i])
-                        visited[g[i]] = True
+            if a in g:
+                for i in g:
+                    if visited[i] == False:
+                        visited[i] = True
+                        deq.append(i)
 
-N, M, V = map(int, sys.stdin.readline().split())
+N,M,V = map(int, sys.stdin.readline().split())
+
 edge = [[] for _ in range(M)]
-
 for i in range(M):
     edge[i]=list(map(int, sys.stdin.readline().split()))
 
 edge = sorted(edge,key= lambda x:sorted([x[0],x[1]]))
-Visited = [False] * (N+1)
-dfs(edge,V,Visited)
+visited = [False] * (N+1)
+dfs(edge, V, visited)
 print()
-Visited = [False] * (N+1)
-bfs(edge,V,Visited)
+visited = [False] * (N+1)
+bfs(edge, V, visited)
