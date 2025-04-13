ec39ff0beac64d6b56935442659322f7a2e34cf7
이승준 <72748734+HiLeeS@users.noreply.github.com>
2025-04-13 21:09:51 +0900

    [Bronze I] Title: 평균, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.py
+++ 백준/Bronze/1546. 평균/평균.py
@@ -3,9 +3,6 @@ n = int(input())
 
 scores = list(map(int, input().split()))    #띄어쓰기로 구분
 M = max(scores)
+sum = sum(scores)
 
-result = 0
-for i in scores:
-    result += i / M * 100
-
-print(result / n)
+print(sum * 100 / M / n)
