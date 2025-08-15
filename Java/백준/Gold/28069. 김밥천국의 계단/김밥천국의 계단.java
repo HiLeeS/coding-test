6c8ca2ebc863ef6de49701a4ef677c53674a0994
이승준 <72748734+HiLeeS@users.noreply.github.com>
2025-08-15 16:18:32 +0900

    [Gold V] Title: 김밥천국의 계단, Time: 136 ms, Memory: 17996 KB -BaekjoonHub

---
+++ Java/백준/Gold/28069. 김밥천국의 계단/김밥천국의 계단.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        int[] dp = new int[N + 1];
+        Arrays.fill(dp, Integer.MAX_VALUE); // INF 대신 사용
+        dp[0] = 0;
+
+        for (int i = 0; i <= N; i++) {
+            if (dp[i] == Integer.MAX_VALUE) continue;
+
+            if (i + 1 <= N && dp[i + 1] > dp[i] + 1)
+                dp[i + 1] = dp[i] + 1;
+
+            int j = i + (i / 2);
+            if (j <= N && dp[j] > dp[i] + 1)
+                dp[j] = dp[i] + 1;
+        }
+
+        System.out.print(dp[N] <= K ? "minigimbob" : "water");
+    }
+}
