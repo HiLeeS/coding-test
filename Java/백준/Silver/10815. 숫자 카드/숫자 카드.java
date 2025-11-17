a6c07bbe88c0c10ca0585172b0b86053bf0dc92d
이승준 <72748734+HiLeeS@users.noreply.github.com>
2025-11-17 19:47:32 +0900

    [Silver V] Title: 숫자 카드, Time: 840 ms, Memory: 135528 KB -BaekjoonHub

--- Java/백준/Silver/10815. 숫자 카드/숫자 카드.java
+++ Java/백준/Silver/10815. 숫자 카드/숫자 카드.java
@@ -4,25 +4,21 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        StringBuilder sb = new StringBuilder();
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(br.readLine());
         Set<Integer> set = new HashSet<>();
-        
-        for(int i = 0; i<n; i++){
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < N; i++){
             set.add(Integer.parseInt(st.nextToken()));
         }
         
-        int m = Integer.parseInt(br.readLine());
+        int M = Integer.parseInt(br.readLine());
         st = new StringTokenizer(br.readLine());
-        for(int i = 0; i<m; i++){
+        for(int i = 0; i < M; i++){
             int num = Integer.parseInt(st.nextToken());
-            if(set.contains(num)){
-                sb.append("1 ");
-            }
-            else{
-                sb.append("0 ");
-            }
+            
+            if(set.contains(num)) sb.append(1+" ");
+            else sb.append(0+" ");
         }
         
         System.out.print(sb);
