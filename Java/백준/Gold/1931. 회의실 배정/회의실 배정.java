df93938459e99da8f55444b99987d00346dab9ca
이승준 <72748734+HiLeeS@users.noreply.github.com>
2025-11-18 17:31:51 +0900

    [Gold V] Title: 회의실 배정, Time: 476 ms, Memory: 44100 KB -BaekjoonHub

---
+++ Java/백준/Gold/1931. 회의실 배정/회의실 배정.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int N = Integer.parseInt(br.readLine());
+        
+        int[][] arr = new int[N][2];
+        
+        for(int i = 0; i < N; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
+        }
+        
+        Arrays.sort(arr, (a, b) -> {
+            if (a[1] == b[1]) return a[0] - b[0];
+            return a[1] - b[1];
+        });
+        
+        int time = -1;
+        int count = 0;
+        for(int i = 0; i < N; i++){
+            if(time <= arr[i][0]){
+                time = arr[i][1];
+                count++;
+            }
+        }
+        
+        System.out.print(count);
+        
+    }
+}
