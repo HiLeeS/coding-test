5ed35c9dae351bb50357a3ff867f06c91dbfc06b
이승준 <72748734+HiLeeS@users.noreply.github.com>
2025-11-18 21:50:24 +0900

    [Silver III] Title: 2×n 타일링, Time: 172 ms, Memory: 17736 KB -BaekjoonHub

---
+++ Java/백준/Silver/11726. 2×n 타일링/2×n 타일링.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        if (n == 1) {
+            System.out.println(1);
+            return;
+        }
+
+        long[] dp = new long[n+1];
+
+        dp[1] = 1;
+        dp[2] = 2;
+
+        for (int i = 3; i <= n; i++){
+            dp[i] = (dp[i-1] + dp[i-2]) % 10007;
+        }
+
+        System.out.print(dp[n]);
+    }
+}
